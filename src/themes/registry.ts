aef8379b13d83dc369713646226ba048a873e507
PJ <tykytanjunior@gmail.com>
2026-05-07 19:20:03 +0200

    feat(themes): register batch 1 (sci-fi, vesmirna-lod, priroda, pergamen, nemrtvi, ctyri-zivly)

--- src/themes/registry.ts
+++ src/themes/registry.ts
@@ -2,6 +2,12 @@ import type { Theme, ThemeId, ThemeScope } from './types';
 import { modreNebeTheme } from './themes/modre-nebe';
 import { bilaTheme } from './themes/bila';
 import { temnaCervenTheme } from './themes/temna-cerven';
+import { sciFiTheme } from './themes/sci-fi';
+import { vesmirnaLodTheme } from './themes/vesmirna-lod';
+import { prirodaTheme } from './themes/priroda';
+import { pergamenTheme } from './themes/pergamen';
+import { nemrtviTheme } from './themes/nemrtvi';
+import { ctyriZivlyTheme } from './themes/ctyri-zivly';
 
 export const DEFAULT_THEME: ThemeId = 'modre-nebe';
 
@@ -9,6 +15,12 @@ export const THEMES: Partial<Record<ThemeId, Theme>> = {
   'modre-nebe':   modreNebeTheme,
   'bila':         bilaTheme,
   'temna-cerven': temnaCervenTheme,
+  'sci-fi':       sciFiTheme,
+  'vesmirna-lod': vesmirnaLodTheme,
+  'priroda':      prirodaTheme,
+  'pergamen':     pergamenTheme,
+  'nemrtvi':      nemrtviTheme,
+  'ctyri-zivly':  ctyriZivlyTheme,
 };
 
 export function getTheme(id: string): Theme {
