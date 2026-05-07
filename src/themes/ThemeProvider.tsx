4ea6d6245d24b921a27bf2d5af23ce99e77afaca
PJ <tykytanjunior@gmail.com>
2026-05-07 18:44:28 +0200

    feat(themes): wire useThemeSync into ThemeProvider

--- src/themes/ThemeProvider.tsx
+++ src/themes/ThemeProvider.tsx
@@ -2,6 +2,7 @@ import { useEffect, type PropsWithChildren } from 'react';
 import { useAtomValue } from 'jotai';
 import { themeAtom } from './state';
 import { applyTheme } from './applyTheme';
+import { useThemeSync } from './useThemeSync';
 
 export function ThemeProvider({ children }: PropsWithChildren) {
   const themeId = useAtomValue(themeAtom);
@@ -9,6 +10,8 @@ export function ThemeProvider({ children }: PropsWithChildren) {
   useEffect(() => {
     void applyTheme(themeId);
   }, [themeId]);
+
+  useThemeSync();
 
   return <>{children}</>;
 }
