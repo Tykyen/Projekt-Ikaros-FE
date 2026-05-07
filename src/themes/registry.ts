48ca45df16bcb6268620eeb282e518db22852f78
PJ <tykytanjunior@gmail.com>
2026-05-07 19:22:31 +0200

    feat(themes): register batch 2 (vesmirna-bitva, hospoda, severske-runy, indiane, africke, arabsky-svet)

--- src/themes/registry.ts
+++ src/themes/registry.ts
@@ -8,19 +8,31 @@ import { prirodaTheme } from './themes/priroda';
 import { pergamenTheme } from './themes/pergamen';
 import { nemrtviTheme } from './themes/nemrtvi';
 import { ctyriZivlyTheme } from './themes/ctyri-zivly';
+import { vesmirnaBitvaTheme } from './themes/vesmirna-bitva';
+import { hospodaTheme } from './themes/hospoda';
+import { severskeRunyTheme } from './themes/severske-runy';
+import { indianeTheme } from './themes/indiane';
+import { africkeTheme } from './themes/africke';
+import { arabskySvetTheme } from './themes/arabsky-svet';
 
 export const DEFAULT_THEME: ThemeId = 'modre-nebe';
 
 export const THEMES: Partial<Record<ThemeId, Theme>> = {
-  'modre-nebe':   modreNebeTheme,
-  'bila':         bilaTheme,
-  'temna-cerven': temnaCervenTheme,
-  'sci-fi':       sciFiTheme,
-  'vesmirna-lod': vesmirnaLodTheme,
-  'priroda':      prirodaTheme,
-  'pergamen':     pergamenTheme,
-  'nemrtvi':      nemrtviTheme,
-  'ctyri-zivly':  ctyriZivlyTheme,
+  'modre-nebe':     modreNebeTheme,
+  'bila':           bilaTheme,
+  'temna-cerven':   temnaCervenTheme,
+  'sci-fi':         sciFiTheme,
+  'vesmirna-lod':   vesmirnaLodTheme,
+  'priroda':        prirodaTheme,
+  'pergamen':       pergamenTheme,
+  'nemrtvi':        nemrtviTheme,
+  'ctyri-zivly':    ctyriZivlyTheme,
+  'vesmirna-bitva': vesmirnaBitvaTheme,
+  'hospoda':        hospodaTheme,
+  'severske-runy':  severskeRunyTheme,
+  'indiane':        indianeTheme,
+  'africke':        africkeTheme,
+  'arabsky-svet':   arabskySvetTheme,
 };
 
 export function getTheme(id: string): Theme {
